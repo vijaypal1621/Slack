f1e00bcd23f14a83ba2fd3cabed34f597690e889
unknown <vijay.nsut@gmail.com>
2020-10-31 16:24:34 +0530

    Sidebar Option updated

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -4,7 +4,15 @@ import SidebarOption from './SidebarOption';
 import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
 import CreateIcon from '@material-ui/icons/Create';
 import InsertCommentIcon from '@material-ui/icons/InsertComment';
-
+import InboxIcon from '@material-ui/icons/Inbox';
+import DraftsIcon from '@material-ui/icons/Drafts';
+import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder';
+import PeopleAltIcon from '@material-ui/icons/PeopleAlt';
+import AppsIcon from '@material-ui/icons/Apps';
+import FileCopyIcon from '@material-ui/icons/FileCopy';
+import ExpandLessIcon from '@material-ui/icons/ExpandLess';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import AddIcon from '@material-ui/icons/Add';
 function Sidebar() {
     return (
         <div className="sidebar">
@@ -16,11 +24,21 @@ function Sidebar() {
                     Vijay PAL
                     </h3>
                 </div>
-                <CreateIcon/>
-                <SidebarOption Icon={InsertCommentIcon} title="Threads" />
-                
-
+                <CreateIcon/>                
             </div>
+            <SidebarOption Icon={InsertCommentIcon} title="Threads" />
+            <SidebarOption Icon={InboxIcon} title="Mentions & Reaction" />
+            <SidebarOption Icon={DraftsIcon} title="Saved Items" />
+            <SidebarOption Icon={BookmarkBorderIcon} title="Channel Browser" />
+            <SidebarOption Icon={PeopleAltIcon} title="People & User Groups" />
+            <SidebarOption Icon={AppsIcon} title="Apps" />
+            <SidebarOption Icon={FileCopyIcon} title="File Browser" />
+            <SidebarOption Icon={ExpandLessIcon} title="Show Less" />
+            <hr />
+            <SidebarOption Icon={ExpandMoreIcon} title="Channels" />
+            <hr />
+            <SidebarOption Icon={AddIcon} title="Add Icon" />
+            {/* Connect to db and and list all the channnels */}
         </div>
     )
 }
--- src/SidebarOption.js
+++ src/SidebarOption.js
@@ -5,7 +5,7 @@ function SidebarOption({Icon,title}) {
     return (
         <div className="sidebarOption">
             {Icon && <Icon className="sidebarOption__icon"/>}
-            
+            {Icon ? (<h3>{title}</h3>) : (<h3 className="sidebarOption__channel"> <span className="sidebarOption__hash">#</span>{title}</h3>) }
         </div>
     )
 }
