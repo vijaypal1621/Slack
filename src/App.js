9e13b1c5e9eee50daa825c0fbdfeb3830c4a0c9d
unknown <vijay.nsut@gmail.com>
2020-10-31 19:45:29 +0530

    Route for chats added

--- src/App.js
+++ src/App.js
@@ -3,17 +3,29 @@ import React from 'react';
 import './App.css';
 import Sidebar from './Sidebar';
 import Header from './Header';
+import {BrowserRouter as Router, Switch,Route} from 'react-router-dom';
 
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
+      <Router>
         <Header />
-       <div className="app__body">
-           {/* Sidebar */}
-           <Sidebar />
-           {/* React-Router ->chat-screen*/}
-       </div>
+        <div className="app__body">
+            {/* Sidebar */}
+            <Sidebar />
+            <Switch>
+              <Route path="/rooms/:roomId">
+                {/* <Chat/> */}
+              </Route>
+              <Route path="/">
+                <h1>Welcome</h1>
+              </Route>
+            </Switch>
+
+            {/* React-Router ->chat-screen*/}
+        </div>
+        </Router>
    </div>
   );
 }
